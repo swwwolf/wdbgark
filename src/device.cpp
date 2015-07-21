80eae51a5944e24e0dd3586d80c6dd30e56ea114
swwwolf <swwwolf@gmail.com>
2015-07-21 23:57:21 +0300

    WDbgArkDevice class implemented

---
+++ src/device.cpp
@@ -0,0 +1,109 @@
+/*
+    * WinDBG Anti-RootKit extension
+    * Copyright © 2013-2015  Vyacheslav Rusakoff
+    * 
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    * 
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+    * 
+    * You should have received a copy of the GNU General Public License
+    * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    * This work is licensed under the terms of the GNU GPL, version 3.  See
+    * the COPYING file in the top-level directory.
+*/
+
+#include <map>
+#include <sstream>
+#include <memory>
+
+#include "device.hpp"
+#include "symcache.hpp"
+#include "manipulators.hpp"
+#include "objhelper.hpp"
+
+namespace wa {
+
+WDbgArkDevice::WDbgArkDevice(const std::shared_ptr<WDbgArkSymCache> &sym_cache)
+    : m_inited(false),
+      m_devices_list(),
+      m_sym_cache(sym_cache),
+      m_obj_helper(new WDbgArkObjHelper(m_sym_cache)),
+      out(),
+      warn(),
+      err() {
+    if ( !m_obj_helper->IsInited() ) {
+        err << wa::showminus << __FUNCTION__ << ": failed to initialize WDbgArkObjHelper" << endlerr;
+        return;
+    }
+
+    auto info = m_obj_helper->GetObjectsInfo();
+
+    if ( FAILED(info.first) ) {
+        err << wa::showminus << __FUNCTION__ << ": GetObjectsInfo failed" << endlerr;
+        return;
+    }
+
+    for ( auto object_info : info.second ) {
+        if ( object_info.second.type == "Device" ) {
+            m_devices_list[object_info.second.object.m_Offset] = object_info.second.object;
+        }
+    }
+
+    if ( m_devices_list.empty() ) {
+        err << wa::showminus << __FUNCTION__ << ": no devices found" << endlerr;
+        return;
+    }
+
+    for ( auto device : m_devices_list ) {
+        try {
+            ExtRemoteTyped next_device = device.second.Field("NextDevice");
+            ExtRemoteTyped attached_to = device.second.Field("DeviceObjectExtension").Field("AttachedTo");
+            ExtRemoteTyped attached_device = device.second.Field("AttachedDevice");
+
+            // loop "attached to" field and "next" fields
+            while ( attached_to.m_Offset ) {
+                m_devices_list[attached_to.m_Offset] = attached_to;
+
+                ExtRemoteTyped next_device_to = attached_to.Field("NextDevice");
+
+                while ( next_device_to.m_Offset ) {
+                    m_devices_list[next_device_to.m_Offset] = next_device_to;
+                    next_device_to = next_device_to.Field("NextDevice");
+                }
+
+                attached_to = attached_to.Field("DeviceObjectExtension").Field("AttachedTo");
+            }
+
+            // loop "attached" field and "next" fields
+            while ( attached_device.m_Offset ) {
+                m_devices_list[attached_device.m_Offset] = attached_device;
+
+                ExtRemoteTyped next_device_attached = attached_device.Field("NextDevice");
+
+                while ( next_device_attached.m_Offset ) {
+                    m_devices_list[next_device_attached.m_Offset] = next_device_attached;
+                    next_device_attached = next_device_attached.Field("NextDevice");
+                }
+
+                attached_device = attached_device.Field("AttachedDevice");
+            }
+
+            // loop "next" fields
+            while ( next_device.m_Offset ) {
+                m_devices_list[next_device.m_Offset] = next_device;
+                next_device = next_device.Field("NextDevice");
+            }
+        } catch ( const ExtRemoteException& ) {}
+    }
+
+    m_inited = true;
+}
+
+}   // namespace wa
---
+++ src/device.hpp
@@ -0,0 +1,66 @@
+/*
+    * WinDBG Anti-RootKit extension
+    * Copyright © 2013-2015  Vyacheslav Rusakoff
+    * 
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    * 
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+    * 
+    * You should have received a copy of the GNU General Public License
+    * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    * This work is licensed under the terms of the GNU GPL, version 3.  See
+    * the COPYING file in the top-level directory.
+*/
+
+//////////////////////////////////////////////////////////////////////////
+//  Include this after "#define EXT_CLASS WDbgArk" only
+//////////////////////////////////////////////////////////////////////////
+
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+#ifndef DEVICE_HPP_
+#define DEVICE_HPP_
+
+#include <engextcpp.hpp>
+
+#include <map>
+#include <sstream>
+#include <memory>
+
+#include "symcache.hpp"
+#include "objhelper.hpp"
+
+namespace wa {
+
+class WDbgArkDevice {
+ public:
+    explicit WDbgArkDevice(const std::shared_ptr<WDbgArkSymCache> &sym_cache);
+    WDbgArkDevice() = delete;
+
+    bool IsInited(void) const { return m_inited; }
+
+ private:
+    bool m_inited;
+    std::map<unsigned __int64, ExtRemoteTyped> m_devices_list;  // offset : object
+    std::shared_ptr<WDbgArkSymCache>           m_sym_cache;
+    std::unique_ptr<WDbgArkObjHelper>          m_obj_helper;
+    //////////////////////////////////////////////////////////////////////////
+    // output streams
+    //////////////////////////////////////////////////////////////////////////
+    std::stringstream out;
+    std::stringstream warn;
+    std::stringstream err;
+};
+
+}   // namespace wa
+
+#endif  // DEVICE_HPP_
